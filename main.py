67f37ea69f23c56fc45aac2daa10f441fcce4d14
oddnaryhie <namansingh00999@gmail.com>
2026-06-19 11:52:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pyautogui
-while True:
-    a =  pyautogui.position()
-    print(a)
-    # 736, 877 
-    # 641,98 to 640,782
+import time
+import pyperclip
+
+# Step 1: click the whatsaap icon
+pyautogui.click(1155, 874,  button='left')
+time.sleep(1)
+
+# Step 2: drag to select text
+pyautogui.moveTo(431, 137)
+pyautogui.dragTo(1481, 779, duration=1.0, button='left')
+
+
+# Step 3: copy selected text
+pyautogui.hotkey('ctrl', 'c')
+
+time.sleep(0.5)
+
+# Step 4: get clipboard content into variable
+copied_text = pyperclip.paste()
+
+print("Copied Text:")
+print(copied_text)
